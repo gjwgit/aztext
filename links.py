c2f72ec435f6e349da2f92a7a2eedfcfe2ea4193
Graham <Graham.Williams@togaware.com>
2019-08-07 11:03:44 +0800

    Add links command

---
+++ links.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# Author: Graham.Williams@microsoft.com
+#
+# A command line script to analyze text.
+#
+# ml links <sentence>
+# 
+# https://docs.microsoft.com/en-us/azure/cognitive-services/text-analytics/
+#   quickstarts/python-sdk
+#
+
+# ----------------------------------------------------------------------
+# Setup
+# ----------------------------------------------------------------------
+
+# Import the required libraries.
+
+import sys
+import os
+import argparse
+
+from textwrap import fill
+from mlhub.pkg import azkey
+
+# pip3 install --upgrade --user azure-cognitiveservices-language-textanalytics
+
+from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient
+from msrest.authentication import CognitiveServicesCredentials
+
+# Defaults.
+
+SERVICE = "Text Analytics"
+KEY_FILE  = os.path.join(os.getcwd(), "private.txt")
+
+# ----------------------------------------------------------------------
+# Parse command line arguments
+# ----------------------------------------------------------------------
+
+option_parser = argparse.ArgumentParser(add_help=False)
+
+option_parser.add_argument(
+    'sentence',
+    nargs="*",
+    help='sentence to analyse')
+
+args = option_parser.parse_args()
+
+# ----------------------------------------------------------------------
+# Request subscription key and endpoint from user.
+# ----------------------------------------------------------------------
+
+key, endpoint = azkey(KEY_FILE, SERVICE, verbose=False, baseurl=True)
+credentials   = CognitiveServicesCredentials(key)
+client        = TextAnalyticsClient(endpoint=endpoint, credentials=credentials)
+
+# ------------------------------------------------------------------------
+# Helper function
+# ------------------------------------------------------------------------
+
+def analyseText(txt):
+    documents = [{ 'id': '1', 'text': txt }]
+    response  = client.detect_language(documents=documents)
+
+    l    = response.documents[0]
+    dl   = l.detected_languages[0]
+    lang = dl.iso6391_name
+
+    documents = [{ 'id': '1', 'language': lang, 'text': txt }]
+    response  = client.entities(documents=documents)
+
+    links = []
+    offsets = []
+    lengths = []
+    for es in response.documents:
+        for e in es.entities:
+            m = e.matches[0]
+            if m.wikipedia_score != None:
+                links.insert(0, e.wikipedia_url)
+                offsets.insert(0, m.offset)
+                lengths.insert(0, m.length)
+
+    for i, url in enumerate(links):
+        txt = txt[0:offsets[i]] + f"<a href=\"{url}\" target=\"_blank\">" + txt[offsets[i]:offsets[i]+lengths[i]] + "</a>" + txt[offsets[i]+lengths[i]:]
+
+    print(fill(txt))
+        
+# ------------------------------------------------------------------------
+# Obtain text and analyze.
+# ------------------------------------------------------------------------
+
+txt = " ".join(args.sentence)
+
+if txt == "":
+
+    txt = sys.stdin.read()
+    analyseText(txt)
+
+
+    #    prompt = "Enter text to be analysed\nQuit with Ctrl-d, Output conf,lang,sentiment,phrases,entities):\n> "
+
+#    try:
+#        txt = input(prompt)
+#    except EOFError:
+#        print()
+#        sys.exit(0)
+
+ #   while txt != '':
+
+
+#     try:
+#            print()
+#            txt = input(prompt)
+#        except EOFError:
+#            print()
+#            sys.exit(0)
+else:
+    analyseText(txt)
